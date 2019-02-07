488eeec3706a85591ecb7c3a3eafeb9945b7748c
Vlad <vladalin123456@gmail.com>
2019-02-07 17:26:37 +0200

    Updates

---
+++ precache-manifest.2f68b85846c42a1d6b8665afe06fe0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "a67d03d6ddc85ca6a69e",
+    "url": "/contactmanager/static/js/main.a67d03d6.chunk.js"
+  },
+  {
+    "revision": "6f7e2b185310544987d7",
+    "url": "/contactmanager/static/js/1.6f7e2b18.chunk.js"
+  },
+  {
+    "revision": "a67d03d6ddc85ca6a69e",
+    "url": "/contactmanager/static/css/main.c4075b6e.chunk.css"
+  },
+  {
+    "revision": "6f7e2b185310544987d7",
+    "url": "/contactmanager/static/css/1.f5777e98.chunk.css"
+  },
+  {
+    "revision": "a08b618226ad1f4650281c533da0925c",
+    "url": "/contactmanager/index.html"
+  }
+];
